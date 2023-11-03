8b7ad3121c2231693180d29cc732269a24c53493
sadaf <sadaf20043006@gmail.com>
2023-11-03 10:33:06 +0530

    feat: integrated handlebars to render data from backend

--- JS/edgechains/examples/src/app.controller.ts
+++ JS/edgechains/examples/src/app.controller.ts
@@ -1,7 +1,14 @@
-import { Body, Controller, Get, HttpCode, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Render,
+  HttpCode,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { hydeSearchAdaEmbedding } from './hydeExample/hydeExample';
-
 
 @Controller()
 export class AppController {
@@ -9,23 +16,49 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    hydeSearchAdaEmbedding("hello");
+    hydeSearchAdaEmbedding('hello');
     return this.appService.getHello();
   }
 
-  @Post("/hyde-search/query-rrf")
+  @Post('/hyde-search/query-rrf')
   @HttpCode(200)
-  hydeSearch(@Query() params:any, @Body() query: any){
+  hydeSearch(@Query() params: any, @Body() query: any) {
     const arkRequest = {
-      topK : params.topK,
-      metadataTable : query.metadataTable,
-      query : query.query,
-      textWeight : query.textWeight,
-      similarityWeight : query.similarityWeight,
-      dateWeight : query.dateWeight,
-      orderRRF : query.orderRRF
-    }
+      topK: params.topK,
+      metadataTable: query.metadataTable,
+      query: query.query,
+      textWeight: query.textWeight,
+      similarityWeight: query.similarityWeight,
+      dateWeight: query.dateWeight,
+      orderRRF: query.orderRRF,
+    };
     return hydeSearchAdaEmbedding(arkRequest);
+  }
 
+  @Get('fragments')
+  @Render('fragments')
+  fragments() {
+    return {
+      messageItem: [
+        { item: { message: 'Message 1' } },
+        { item: { message: 'Message 2' } },
+      ],
+      chatItem: [
+        { chat: { heading: 'Chat 1' } },
+        { chat: { heading: 'Chat 2' } },
+      ],
+    };
+  }
+
+  @Get('index')
+  @Render('index')
+  index() {
+    return {
+      chats: [{ chat: { heading: 'Chat 1' } }, { chat: { heading: 'Chat 2' } }],
+      messages: [
+        { item: { message: 'Message 1' } },
+        { item: { message: 'Message 2' } },
+      ],
+    };
   }
 }
--- JS/edgechains/examples/src/main.ts
+++ JS/edgechains/examples/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setBaseViewsDir('views');
+  app.setViewEngine('hbs');
   await app.listen(8080);
 }
 bootstrap();
